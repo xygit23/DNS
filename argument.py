d70dcb64f7edbbacb1197d5c8defbad4783bb9b0
xygit23 <122794046+xygit23@users.noreply.github.com>
2023-04-13 16:39:22 +0800

    Add files via upload

--- argument.py
+++ argument.py
@@ -4,10 +4,11 @@ import time
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="citeseer", help="cora, citeseer, pubmed")
-    parser.add_argument("--models", nargs='+', default=['GCN', 'lp', 'cotraining', 'selftraining', 'union', 'intersection'], help="'GCN', 'lp', 'cotraining', 'selftraining', 'union', 'intersection'")
+    parser.add_argument("--dataset", type=str, default="citeseer", help="cora, citeseer, pubmed, photo")
+    parser.add_argument("--models", nargs='+', default=['GCN'], help="'GCN', 'lp', 'cotraining', 'selftraining', 'union', 'intersection'")
+    parser.add_argument("--label_rate", type=float, default=4, help="The rate of labeled samples(%) for recommend parameters.")
 
-    parser.add_argument("--repeating", type=int, default=10, help="The number of repeat times.")
+    parser.add_argument("--repeating", type=int, default=1, help="The number of repeat times.")
     parser.add_argument("--runs", type=int, default=10, help="The number of runs.")
     parser.add_argument("--test_size", type=int, default=1000)
 
@@ -18,7 +19,6 @@ def parse_args():
     # The following parameters is optional since we provided the recommend parameters.
     # parser.add_argument("--train_size", type=int, default=18, help="The number of labeled samples in per class")
     # parser.add_argument("--layers", nargs='+', default=[16], help="The number of hidden units of each layer of the GCN.")
-    # parser.add_argument("--label_rate", type=int, default=1, help="The rate of labeled samples(%) for recommend parameters.")
     # parser.add_argument("--dc", type=float, default=2.5, help="Bandwidth parameter for GOLF.")
     # parser.add_argument("--lt_num", type=int, default=79, help="The number of Leading Tree.")
     # parser.add_argument("--k", type=int, default=11, help="The number of samples selected from per class.")
